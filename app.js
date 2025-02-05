471bdb5e8beccc4a2a9b704cbbedd2ed7014bda4
AnnyPerez12 <yoletzysantiago2014@gmail.com>
2025-02-04 20:21:51 -0400

    Modificacion de mi api

--- app.js
+++ app.js
@@ -1,37 +1,88 @@
-const Express=require("express")
-const Cors=require("cors")
+const Express = require("express")
+const Cors = require("cors")
 
-const app=Express()
+const app = Express()
 app.use(Cors())
 app.use(Express.json())
 
 // Tu código va desde aquí ⬇️
-const Estudiantes = [
-    {
-      id: 22,
-      title: "Joelby Salcedo",
-      description: "Alto y muy delgado",
-      completed: false,
-    },
-    {
-      id: 20,
-      title: "Anny Perez",
-      description: "Una bajita morena",
-      completed: true,
-    },
-    app.get("/",(req,res)=>{
-        const output = {
-            code:200,
-            message: "Api en funcionamiento"
-        }
-        return res.send(output).status (200)
-    })
-    app.get("/Estudiantes",(req,res)=>{
-        return res.send(Estudiantes).status(200)
-    })
-    
-  ];
+const tasks = [
+  {
+    id: 1,
+    title: "Diseñar la interfaz de usuario",
+    description: "Crear los mockups y el diseño visual de la aplicación",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Implementar la lógica de negocio",
+    description: "Desarrollar las funciones principales de la aplicación",
+    completed: true,
+  },
+];
+let currentld = tasks.length + 1;
+
+//Crear una nueva tarea con (POST/tasks)
+app.post("/tasks", (req, res) => {
+  const newTask = {
+    id: currentld++,
+    title: req.body.title,
+    description: req.body.description,
+    completed: req.body.completed
+  };
+  tasks.push(newTask);
+  res.status(201).json(newTasks);
+});
+
+//Aqui obtenemos la lista de todoas las tareas a realizar con (GET/tasks)
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+//Vamos a obtener una tarea especifica por su ID con (GET/tasks/:id)
+app.get("/tasks/:id", (req, res) => {
+  const task = tasks.find(t => t.id == req.params.id);
+  if (task) {
+    res.json(task);
+  }
+  else {
+    res.status(404).send("Tarea no encontrada");
+  }
+});
+
+// Actualicemos una tarea ya existente con (PUT/tasks/:id)
+app.put("/tasks/id:", (req, res) => {
+  const task = tasks.find(t => t.id == req, params.id);
+  if (task) {
+    task.title = req.body.title;
+    task.description = req.body.description;
+    task.completed = req.body.completed;
+    res.json(task);
+  }
+  else {
+    res.status(404).send("Tarea no encontrada");
+  }
+});
+
+//Eliminamos una tarea existente con (DELETE/tasks/:id)
+app.delete("/tasks/:id", (req, res) => {
+  const task = tasks.findIndex(t => req.params.id);
+  if (index > -1) {
+    const deletedTask = tasks.splice(index, 1);
+    res.json(deletedTask);
+  }
+  else {
+    res.status(404).send("Tarea no encontrada");
+  }
+});
+
+//Iniciar el servidor por medio del PORT 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Servidor escuchando en puerto $ 3000');
+
+});
 
 // Hasta aquí ⬇⬆️
 
-module.exports={app}
+module.exports = { app }
